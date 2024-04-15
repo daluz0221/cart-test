454fc6c2d4e8b8bedfaa996fb518d8241f086bd4
Luis Felipe <lfelipe.desarrollador@gmail.com>
2024-04-15 15:22:31 -0500

    ajustes

--- cart/settings.py
+++ cart/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 
-ALLOWED_HOSTS = ['blogpipe.xyz']
+ALLOWED_HOSTS = ['blogpipe.xyz', 'www.blogpipe.xyz"']
 DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 # Application definition
